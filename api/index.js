4ee9b7d908c9472d6d9b453606e29949be849634
Vaalarivan2002 <pvaalarivan@gmail.com>
2024-09-19 00:10:18 +0530

    backend port changed

--- api/index.js
+++ api/index.js
@@ -49,7 +49,7 @@ app.use((err, req, res, next) => {
 });
 
 
-const port = process.env.PORT || 8800;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   connect();
   console.log("Backend");
